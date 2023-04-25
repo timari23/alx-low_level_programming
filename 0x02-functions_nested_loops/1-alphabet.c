e8213a7f1663acf2bd69683b82960a0b3537129e
timari23 <timari92@gmail.com>
2023-04-25 13:07:46 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,24 +1,15 @@
-main.h CODE
+#include "main.h"
 
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
 
+void print_alphabet(void)
+{
+	char letter;
 
-#ifndef main_h
-	#define main_h
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 
-
-	int _putchar(char);
-	void print_alphabet(void);
-	void print_alphabet_x10(void);
-	int _islower(int c);
-	int _isalpha(int c);
-	int print_sign(int n);
-	int _abs(int);
-	int print_last_digit(int);
-	void jack_bauer(void);
-	void times_table(void);
-	int add(int, int);
-	void print_to_98(int n);
-	void print_times_table(int n);
-
-
-	#endif
+	_putchar('\n');
+}
